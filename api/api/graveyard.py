869f93e3948615b79968cf6db9542b26424d5dbc
Anthony Hatfield <5w7rzb5jsj@privaterelay.appleid.com>
2026-09-11 11:07:52 -0400

    Modify response format in graveyard.py

--- api/api/graveyard.py
+++ api/api/graveyard.py
@@ -3,10 +3,7 @@ import json
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-Type','application/json')
+        self.send_header('Content-type','application/json')
         self.send_header('Access-Control-Allow-Origin','*')
         self.end_headers()
-        self.wfile.write(json.dumps({
-            "alive":0,"dead":0,"fund":0,"divergence":0,
-            "memories":[]
-        }).encode())
+        self.wfile.write(json.dumps([]).encode())
